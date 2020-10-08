a28d340b519de67a2bd8bdcb83770c67c7b9cad6
larsverp2803@gmail.com <larsverp2803@gmail.com>
2020-10-08 10:44:13 +0200

    Created test endpoint
    The `/hi` endpoint will return "Hi friend!". This is my first endpoint and will serve as a test.

--- src/main/java/com/example/KVWNuenen/KvwNuenenApplication.java
+++ src/main/java/com/example/KVWNuenen/KvwNuenenApplication.java
@@ -2,6 +2,8 @@ package com.example.KVWNuenen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class KvwNuenenApplication {
@@ -10,4 +12,11 @@ public class KvwNuenenApplication {
 		SpringApplication.run(KvwNuenenApplication.class, args);
 	}
 
+	@RestController
+	public class testController{
+		@GetMapping("/hi")
+		public String sayHi(){
+			return "Hi friend!";
+		}
+	}
 }
