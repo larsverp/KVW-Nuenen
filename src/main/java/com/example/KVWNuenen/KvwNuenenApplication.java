f42eca2c2bc6a1827a0edb3c1982d909cfd88159
larsverp2803@gmail.com <larsverp2803@gmail.com>
2020-10-08 10:36:07 +0200

    Setting up project config
    Setting up the Sring boot project configuration.

---
+++ src/main/java/com/example/KVWNuenen/KvwNuenenApplication.java
@@ -0,0 +1,13 @@
+package com.example.KVWNuenen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvwNuenenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvwNuenenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KVWNuenen/KvwNuenenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KVWNuenen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvwNuenenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
